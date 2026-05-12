bf8f0e62360e2a49484a88395d5111b65a3467a7
luca <luca.romano@t-online.de>
2026-05-12 14:59:47 +0200

    chore :tada: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import React from 'react'
+import Template from "./Template.jsx";
+
+function App() {
+    return (
+        <Template/>
+    )
+}
+
+export default App
---
+++ src/Template.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Container, Typography, Paper, Box } from '@mui/material';
+
+export default function Template() {
+    return (
+        <Container maxWidth="md">
+            <Typography
+                variant="h4"
+                gutterBottom
+                align="center"
+                sx={{ fontFamily: 'monospace' }}
+            >
+                --- AppBar ---
+            </Typography>
+            <Typography variant="h6" gutterBottom>
+                Hier soll eine umrandete Überschrift hin.
+            </Typography>
+            <Paper elevation={3} sx={{ padding: 3, marginTop: 4 }}>
+                <Typography variant="h5" gutterBottom>
+                    Willkommen auf der Seite
+                </Typography>
+
+                <Box sx={{ mt: 3, p: 2, bgcolor: '#f5f5f5', borderRadius: 2 }}>
+                    <Typography variant="subtitle1">
+                        Zusatzinfo oder Sektion
+                    </Typography>
+                </Box>
+            </Paper>
+            <Paper elevation={3} sx={{ padding: 3, marginTop: 4 }}>
+                <Typography variant="h5" gutterBottom>
+                    Willkommen auf der Seite
+                </Typography>
+
+                <Box sx={{ mt: 3, p: 2, bgcolor: '#f5f5f5', borderRadius: 2 }}>
+                    <Typography variant="subtitle1">
+                        Zusatzinfo oder Sektion
+                    </Typography>
+                </Box>
+            </Paper>
+        </Container>
+    );
+}
